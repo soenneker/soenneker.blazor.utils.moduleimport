ae556d38279105e2907d5a1d941bfe1b6c1e660b
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:34:44 +0000

    xUnit v3

--- test/Soenneker.Blazor.Utils.ModuleImport.Tests/ModuleImportUtilTests.cs
+++ test/Soenneker.Blazor.Utils.ModuleImport.Tests/ModuleImportUtilTests.cs
@@ -1,7 +1,7 @@
 using Soenneker.Blazor.Utils.ModuleImport.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-using Xunit.Abstractions;
+
 
 namespace Soenneker.Blazor.Utils.ModuleImport.Tests;
 
